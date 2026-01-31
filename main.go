fe80c158bea13bede32b3fc6b76215663b79d7e4
lucasmoraist <luksmnt1101@gmail.com>
2026-01-31 11:23:38 -0300

    feat: move pizza data to global variable and implement POST endpoint for adding pizzas

--- main.go
+++ main.go
@@ -6,23 +6,34 @@ import (
 	"pizzaria/models"
 )
 
+var pizzas = []models.Pizza{
+	{ID: 1, Nome: "Margherita", Preco: 25.0},
+	{ID: 2, Nome: "Pepperoni", Preco: 30.0},
+	{ID: 3, Nome: "Quatro Queijos", Preco: 35.0},
+}
+
 func main() {
 	router := gin.Default()
 
 	router.GET("/pizzas", getPizza)
+	router.POST("/pizzas", postPizzas)
 
 	router.Run()
 }
 
 func getPizza(c *gin.Context) {
-	pizzas := []models.Pizza{
-		{ID: 1, Nome: "Margherita", Preco: 25.0},
-		{ID: 2, Nome: "Pepperoni", Preco: 30.0},
-		{ID: 3, Nome: "Quatro Queijos", Preco: 35.0},
-	}
-
 	c.JSON(200, gin.H{
 		"pizzas": pizzas,
 	})
 	fmt.Println("Pizzas retornadas com sucesso")
+}
+
+func postPizzas(c *gin.Context) {
+	var newPizza models.Pizza
+	if err := c.ShouldBindJSON(&newPizza); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	pizzas = append(pizzas, newPizza)
 }
