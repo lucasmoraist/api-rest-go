c94ef9af0b420235ecdb514ccf8962036a829f27
lucasmoraist <luksmnt1101@gmail.com>
2026-02-03 09:08:20 -0300

    feat: return new pizza data in response after adding a pizza

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func postPizzas(c *gin.Context) {
 	newPizza.ID = len(pizzas) + 1
 	pizzas = append(pizzas, newPizza)
 	savePizza()
+	c.JSON(201, newPizza)
 }
 
 func getPizzasById(c *gin.Context) {
